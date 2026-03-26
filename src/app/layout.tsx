eee9a6d86a4d7695d9d4810483ad4630d16dfc93
Kevin Downey <downey.kevin@gmail.com>
2026-03-26 18:19:24 -0400

    Add GTM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,12 @@
 import { type Metadata } from 'next'
+import Script from 'next/script'
 
 import { Providers } from '@/app/providers'
 import { getSiteUrl } from '@/lib/site-url'
 
 import '@/styles/tailwind.css'
 
+const gtmId = 'GTM-M644WFD2'
 const siteUrl = getSiteUrl()
 const siteName = 'Arche Documentation'
 const siteTitle = 'Arche Documentation'
@@ -127,9 +129,26 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 
   return (
     <html lang="en" className="h-full" suppressHydrationWarning>
+      <head>
+        <Script id="google-tag-manager" strategy="beforeInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${gtmId}');`}
+        </Script>
+      </head>
       <body
         className="min-h-full bg-[color:var(--docs-bg)] text-[color:var(--docs-text)] antialiased"
       >
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
